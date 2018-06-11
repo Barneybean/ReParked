be1531973e6a92555886ee386ea9425acdd9a27c
Jose Castellanos <josecastellanos@Joses-MacBook-Pro-2.local>
2018-06-10 23:17:34 -0700

    more changes to listing

--- public/js/listings.js
+++ public/js/listings.js
@@ -1,48 +1,80 @@
-$( function() {
-    var dateFormat = "mm/dd/yy",
-      from = $( "#from" )
-        .datepicker({
-          defaultDate: "+1w",
-          changeMonth: true,
-          numberOfMonths: 1
-        })
-        .on( "change", function() {
-          to.datepicker( "option", "minDate", getDate( this ) );
-        }),
-      to = $( "#to" ).datepicker({
-        defaultDate: "+1w",
-        changeMonth: true,
-        numberOfMonths: 1
-      })
-      .on( "change", function() {
-        from.datepicker( "option", "maxDate", getDate( this ) );
-      });
+// $( function() {
+//     var dateFormat = "mm/dd/yy",
+//       from = $( "#from" )
+//         .datepicker({
+//           defaultDate: "+1w",
+//           changeMonth: true,
+//           numberOfMonths: 1
+//         })
+//         .on( "change", function() {
+//           to.datepicker( "option", "minDate", getDate( this ) );
+//         }),
+//       to = $( "#to" ).datepicker({
+//         defaultDate: "+1w",
+//         changeMonth: true,
+//         numberOfMonths: 1
+//       })
+//       .on( "change", function() {
+//         from.datepicker( "option", "maxDate", getDate( this ) );
+//       });
  
-    function getDate( element ) {
-      var date;
-      try {
-        date = $.datepicker.parseDate( dateFormat, element.value );
-      } catch( error ) {
-        date = null;
-      }
+//     function getDate( element ) {
+//       var date;
+//       try {
+//         date = $.datepicker.parseDate( dateFormat, element.value );
+//       } catch( error ) {
+//         date = null;
+//       }
  
-      return date;
-    }
+//       return date;
+//     }
 
 
-//geo code begin
- //  var mapsApi = "AIzaSyA2n1eGuHjUHi0ZhQeolicSpu2RrmhQoCg";
- function initMap() {
-    // The location of Uluru
-    var uluru = {lat: 37.6688, lng: 122.0808};
-    // The map, centered at Uluru
-    var map = new google.maps.Map(
-        document.getElementById('map'), {zoom: 4, center: uluru});
-    // The marker, positioned at Uluru
-    var marker = new google.maps.Marker({position: uluru, map: map});
-  } 
+// geo code begin
+
+//   var mapsApi = "AIzaSyA2n1eGuHjUHi0ZhQeolicSpu2RrmhQoCg";
+
+//  function initMap() {
+//     // The location of Uluru
+//     var uluru = {lat: 37.6688, lng: 122.0808};
+//     // The map, centered at Uluru
+//     var map = new google.maps.Map(
+//         document.getElementById('#map'), {zoom: 4, center: uluru});
+//     // The marker, positioned at Uluru
+//     var marker = new google.maps.Marker({position: uluru, map: map});
+//   } 
+// initMap();
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById('map');
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
+
+
   
-  geocode();
+
+function initMap() {
+  // The location of Uluru
+  var uluru = {lat: 37.874537, lng: -122.275694,
+  //             lat: 37.873845, lng: -122.276870,
+  //             lat: 37.875035, lng: -122.274702  
+  };
+  // The map, centered at Uluru
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 13, center: uluru});
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({position: uluru , map: map});
+}
+
+  
   function geocode(){
           var location = '22 Main st Boston MA';
       axios.get('https://maps.googleapis.com/maps/api/geocode/json?',{
@@ -56,10 +88,6 @@ $( function() {
           
           var address = response.data.results[0].formatted_address ;
   
-          // var addressOutput = document.getElementById('#text');
-        //   document.getElementById('#formatted-address').innerHTML = address;
-        //   document.getElementById('#formatted-address').innerHTML
-  
       })
       .catch(function(error){
           conosle.log(error)
@@ -70,5 +98,4 @@ $( function() {
 
 //geo code end 
 
-});
-
+// });
