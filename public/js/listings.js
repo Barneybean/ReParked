1046f73f813afe8f56f600cf42128389ce737467
Jose Castellanos <josecastellanos@calvisitor-10-105-194-111.calvisitor.1918.berkeley.edu>
2018-06-11 10:47:06 -0700

    before changes

--- public/js/listings.js
+++ public/js/listings.js
@@ -30,20 +30,34 @@
 //     }
 
 
+
+// This code makes the card clickable
+ function addClick(){
+     var card = document.getElementsByClassName("click");
+     card.innerHTML = "Hello"
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // geo code begin
 
-//   var mapsApi = "AIzaSyA2n1eGuHjUHi0ZhQeolicSpu2RrmhQoCg";
-
-//  function initMap() {
-//     // The location of Uluru
-//     var uluru = {lat: 37.6688, lng: 122.0808};
-//     // The map, centered at Uluru
-//     var map = new google.maps.Map(
-//         document.getElementById('#map'), {zoom: 4, center: uluru});
-//     // The marker, positioned at Uluru
-//     var marker = new google.maps.Marker({position: uluru, map: map});
-//   } 
-// initMap();
 window.onscroll = function() {myFunction()};
 
 var header = document.getElementById('map');
@@ -61,18 +75,20 @@ function myFunction() {
 
   
 
-function initMap() {
-  // The location of Uluru
-  var uluru = {lat: 37.874537, lng: -122.275694,
-  //             lat: 37.873845, lng: -122.276870,
-  //             lat: 37.875035, lng: -122.274702  
-  };
-  // The map, centered at Uluru
-  var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 13, center: uluru});
-  // The marker, positioned at Uluru
-  var marker = new google.maps.Marker({position: uluru , map: map});
-}
+// function initMap() {
+//   // The location of Uluru
+//   var uluru = [{lat: 37.874537, lng: -122.275694, 
+//                 lat:37.872578, lng:-122.272927,
+//                 lat: 37.873827, lng:-122.270386 }];
+//     for(var i = 0; i < uluru.length; i++){
+//         console.log(uluru[1]);
+//     }
+//   // The map, centered at Uluru
+//   var map = new google.maps.Map(
+//       document.getElementById('map'), {zoom: 13, center: uluru[1]});
+//   // The marker, positioned at Uluru
+//   var marker = new google.maps.Marker({position: uluru[1] , map: map});
+// }
 
   
   function geocode(){
@@ -99,3 +115,46 @@ function initMap() {
 //geo code end 
 
 // });
+
+var neighborhoods = [
+    {lat: 37.873827, lng: -122.270386},
+    {lat: 37.872578, lng: -122.272927},
+    {lat: 37.874790, lng: -122.276636 },
+    {lat:37.875035, lng: -122.274702},
+    {lat:37.873845, lng: -122.276870},
+    {lat:37.875035, lng: -122.275694}
+  ];
+
+  var markers = [];
+  var map;
+
+  function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 14,
+      center: {lat: 37.8720, lng: -122.2713}
+    });
+  }
+
+  function drop() {
+    clearMarkers();
+    for (var i = 0; i < neighborhoods.length; i++) {
+      addMarkerWithTimeout(neighborhoods[i], i * 200);
+    }
+  }
+
+  function addMarkerWithTimeout(position, timeout) {
+    window.setTimeout(function() {
+      markers.push(new google.maps.Marker({
+        position: position,
+        map: map,
+        animation: google.maps.Animation.DROP
+      }));
+    }, timeout);
+  }
+
+  function clearMarkers() {
+    for (var i = 0; i < markers.length; i++) {
+      markers[i].setMap(null);
+    }
+    markers = [];
+  }
