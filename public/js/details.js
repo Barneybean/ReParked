e25eebc029ece09cab3170ec12b89b85d130f766
Jonathan Laner <jonathanlaner@gmail.com>
2018-06-12 13:41:12 -0700

    adding details html

--- public/js/details.js
+++ public/js/details.js
@@ -1,1 +1,12 @@
- 
+$('#day-1').val(reservation.bookedDate)
+
+
+for(var i = 0; i < reservations.bookedHour; i++){
+  var hour = reservation.bookedHour[i];
+    $('#1-'+hour).attr('class', highlight)
+}
+
+.highlight {
+  background: yellow;
+}
+
