dc8794702487e61174d3046d6132a16929fc4f6a
William <sewilliam.gao@gmail.com>
2018-06-12 12:13:25 -0700

    added search function in listings page

--- public/js/listings.js
+++ public/js/listings.js
@@ -186,3 +186,147 @@ marker.addListener('click', function() {
       }
     });
   };
+
+
+
+
+
+// adding the login 
+    //----------Sign up & log in------------------------------
+    $("#beHost").on("click", function() {
+       //to prevent submission without value
+      //  event.preventDefault();
+      $("#renterModal").hide();
+      $("#logInModal").hide();
+      $("#hostModal").show();
+    });
+  
+    $("#beRenter").on("click", function() {
+      //to prevent submission without value
+      // event.preventDefault();
+      $("#hostModal").hide();
+      $("#logInModal").hide();
+      $("#renterModal").show();
+   });
+  
+   $("#logInBtn").on("click", function() {
+    //to prevent submission without value
+    // event.preventDefault();
+    $("#logInModal").show();
+  });
+  
+   $("#hostLogInBtn").on("click", function() {
+     $("#hostLogInModal").show();
+   })
+  //-------------sign up & log in ends----------------------------------
+      
+    var loginForm = $("personalModal");
+    var usernameInput = $("#comfirmName")
+    var emailInput = $("#comfirmEmail");
+    var passwordInput = $("#comfirmPw");
+  
+   $("#userBack").on("click", function(event) {
+     event.preventDefault();
+    //  window.location.reload();
+     $("#logInBtn").css("display", "none");
+     $("<div>", {
+       id:"renterInfo",
+       text:"Welcome back, "
+     }).css({
+  
+     })
+      var userData = {
+        username: usernameInput.val().trim(),
+        email: emailInput.val().trim(),
+        password: passwordInput.val().trim()
+      };
+  
+      if (!userData.username || !userData.email || !userData.password) {
+        return;
+      }
+  
+      loginUser(userData.username, userData.email, userData.password);
+      emailInput.val("");
+      emailInput.val("");
+      passwordInput.val("");
+      // window.location.reload();
+    });
+     // loginUser does a post to our "api/login" route and if successful, redirects us the the members page
+    function loginUser(username, email, password) {
+      $.post("/api/login", {
+        username: username,
+        email: email,
+        password: password
+      }).then(function(data) {
+        window.location.replace(data);
+        // If there's an error, log the error
+      }).catch(function(err) {
+        console.log(err);
+      });
+    //  window.location.reload()
+   }
+  
+  
+  //-------------Create Account---------------------------------
+    $("#submit-host").on("click", function() {
+       
+    })
+  
+    $("submit-renter").on("click", function() {
+  
+  
+    })
+
+    //william
+//retrieve session storage - search string *******************
+var searchString = sessionStorage.search;
+// console.log(searchString);
+
+// send it back to get city name
+var addressString = {
+  address: searchString
+}
+
+$.ajax("/api/address", {
+  type: "post",
+  data: addressString
+}).then(function(result) {
+  if(result.city === undefined){
+    $("#display").html("API Error, Please Search Again");
+  }
+  else {
+    console.log (result.city.address_components[3].long_name);
+  }
+});
+
+$("#search").on("submit", function(event) {
+  //to prevent submission without value
+  event.preventDefault();
+  console.log("clicked");
+
+  //clear sessino storage
+  sessionStorage.clear();
+  // get the searched string
+  var searchString = $("#listingsAddress").val().trim();
+   // send it back to get city name
+  var addressString = {
+    address: searchString
+  }
+  console.log(addressString);
+  
+  $.ajax("/api/listingsaddress", {
+    type: "post",
+    data: addressString
+  }).then(function(result) {
+    console.log(result);
+    if(result.city === undefined){
+      $("#display").html("API Error, Please Search Again");
+    }
+    else {
+      
+      $("#display").html("success")
+      console.log (result.city.address_components[3].long_name);
+    }
+  });
+ 
+});
