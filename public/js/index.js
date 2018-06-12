576d6bd39a9b35bcd4e3c9403aba3c4bf562bfe8
Jose Castellanos <josecastellanos@calvisitor-10-105-195-117.calvisitor.1918.berkeley.edu>
2018-06-12 10:26:16 -0700

    stashing

--- public/js/index.js
+++ public/js/index.js
@@ -16,118 +16,114 @@
     <li class="nav-item">
       <a class="nav-link" href="#">Login</a>
     </li>
-</ul> */
-$(document).ready(function() {
+</ul> */$(document).ready(function() {
   //----------Sign up & log in------------------------------
   $("#beHost").on("click", function() {
-     //to prevent submission without value
-    //  event.preventDefault();
-    $("#renterModal").hide();
-    $("#logInModal").hide();
-    $("#hostModal").show();
-  });
-
-  $("#beRenter").on("click", function() {
     //to prevent submission without value
-    // event.preventDefault();
-    $("#hostModal").hide();
-    $("#logInModal").hide();
-    $("#renterModal").show();
+   //  event.preventDefault();
+   $("#renterModal").hide();
+   $("#logInModal").hide();
+   $("#hostModal").show();
  });
 
- $("#logInBtn").on("click", function() {
-  //to prevent submission without value
-  // event.preventDefault();
-  $("#logInModal").show();
+ $("#beRenter").on("click", function() {
+   //to prevent submission without value
+   // event.preventDefault();
+   $("#hostModal").hide();
+   $("#logInModal").hide();
+   $("#renterModal").show();
 });
 
- $("#hostLogInBtn").on("click", function() {
-   $("#hostLogInModal").show();
- })
+$("#logInBtn").on("click", function() {
+ //to prevent submission without value
+ // event.preventDefault();
+ $("#logInModal").show();
+});
+
+$("#hostLogInBtn").on("click", function() {
+  $("#hostLogInModal").show();
+})
 //-------------sign up & log in ends----------------------------------
-    
-  var loginForm = $("personalModal");
-  var usernameInput = $("#comfirmName")
-  var emailInput = $("#comfirmEmail");
-  var passwordInput = $("#comfirmPw");
+   
+ var loginForm = $("personalModal");
+ var usernameInput = $("#comfirmName")
+ var emailInput = $("#comfirmEmail");
+ var passwordInput = $("#comfirmPw");
 
- $("#userBack").on("click", function(event) {
-   event.preventDefault();
-  //  window.location.reload();
-   $("#logInBtn").css("display", "none");
-   $("<div>", {
-     id:"renterInfo",
-     text:"Welcome back, "
-   }).css({
+$("#userBack").on("click", function(event) {
+  event.preventDefault();
+ //  window.location.reload();
+  $("#logInBtn").css("display", "none");
+  $("<div>", {
+    id:"renterInfo",
+    text:"Welcome back, "
+  }).css({
 
-   })
-    var userData = {
-      username: usernameInput.val().trim(),
-      email: emailInput.val().trim(),
-      password: passwordInput.val().trim()
-    };
+  })
+   var userData = {
+     username: usernameInput.val().trim(),
+     email: emailInput.val().trim(),
+     password: passwordInput.val().trim()
+   };
 
-    if (!userData.username || !userData.email || !userData.password) {
-      return;
-    }
+   if (!userData.username || !userData.email || !userData.password) {
+     return;
+   }
 
-    loginUser(userData.username, userData.email, userData.password);
-    emailInput.val("");
-    emailInput.val("");
-    passwordInput.val("");
-    // window.location.reload();
-  });
-   // loginUser does a post to our "api/login" route and if successful, redirects us the the members page
-  function loginUser(username, email, password) {
-    $.post("/api/login", {
-      username: username,
-      email: email,
-      password: password
-    }).then(function(data) {
-      window.location.replace(data);
-      // If there's an error, log the error
-    }).catch(function(err) {
-      console.log(err);
-    });
-  //  window.location.reload()
- }
+   loginUser(userData.username, userData.email, userData.password);
+   emailInput.val("");
+   emailInput.val("");
+   passwordInput.val("");
+   // window.location.reload();
+ });
+  // loginUser does a post to our "api/login" route and if successful, redirects us the the members page
+ function loginUser(username, email, password) {
+   $.post("/api/login", {
+     username: username,
+     email: email,
+     password: password
+   }).then(function(data) {
+     window.location.replace(data);
+     // If there's an error, log the error
+   }).catch(function(err) {
+     console.log(err);
+   });
+ //  window.location.reload()
+}
 
 
 //-------------Create Account---------------------------------
-  $("#submit-host").on("click", function() {
-     
-  })
+ $("#submit-host").on("click", function() {
+    
+ })
 
-  $("submit-renter").on("click", function() {
+ $("submit-renter").on("click", function() {
 
 
-  })
+ })
 //-------------Create Account Ends---------------------------------
 
-  //---------------------search Bar----------------------------- 
-  $("#searchBar").on("submit", function(event) {
-    //to prevent submission without value
-    event.preventDefault();
-    // get the searched string
-    var searchString = $("#address").val().trim();
-    // console.log(searchString);
-    var addressString = {
-      address: searchString
-    }
+ //---------------------search Bar----------------------------- 
+ $("#searchBar").on("submit", function(event) {
+   //to prevent submission without value
+   event.preventDefault();
+   // get the searched string
+   var searchString = $("#address").val().trim();
+   // console.log(searchString);
+   var addressString = {
+     address: searchString
+   }
 
-    $.ajax("/api/address", {
-      type: "post",
-      data: addressString
-    }).then(function(result) {
-      console.log ("searched")
-    });
-  
-  })
+   $.ajax("/api/address", {
+     type: "post",
+     data: addressString
+   }).then(function(result) {
+     console.log ("searched")
+   });
+ 
+ })
 
 
 
 
 });
-
-
-
